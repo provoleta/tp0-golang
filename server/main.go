c31b32f364db98ce72c85eeaab8f0f7dde3a8b40
Tomas <tbuhler@frba.utn.edu.ar>
2024-09-02 03:33:57 +0000

    Prueba de credenciales

--- server/main.go
+++ server/main.go
@@ -10,7 +10,7 @@ func main() {
 
 	mux.HandleFunc("/paquetes", utils.RecibirPaquetes)
 	mux.HandleFunc("/mensaje", utils.RecibirMensaje)
-
+	// aca no hice nd
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		panic(err)
